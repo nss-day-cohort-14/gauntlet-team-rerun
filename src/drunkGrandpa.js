5041b40fdf0660e00fd50a0402ec23bfa370ff1b
Alex Horton <alexhortonmusic@gmail.com>
2016-07-06 00:15:45 -0500

    accidentally worked on master

--- src/drunkGrandpa.js
+++ src/drunkGrandpa.js
@@ -10,4 +10,32 @@ function DrunkGrandpa () {
 	this.patience = 5;
 }
 
+let alcoholicHammer = {
+	name: 'Alcoholic Hammer ', // name
+  damage: 8, // how much it could reduce health
+  snarkyRemark: "Who do I have to disown to get another drink around here (hicCUP!).", // what will show up on the screen
+  use: 2 // how many time in a round this move can be used
+};
+
+let racistComment = {
+	name: 'Racist Comment', // name
+  damage: 15, // how much it could reduce health
+  snarkyRemark: "Well, I'm not a racist, but...(something horrible)", // what will show up on the screen
+  use: 1 // how many time in a round this move can be used
+};
+
+let mumbleBee = {
+	name: 'The Mumble-Bee', // name
+  damage: 5, // how much it could reduce health
+  snarkyRemark: "M-m-may... wh-what? (something unintelligible...)", // what will show up on the screen
+  use: 5 // how many time in a round this move can be used
+};
+
+let boozeOoka = {
+	name: 'Booze-ooka', // name
+  damage: 20, // how much it could reduce health
+  snarkyRemark: "(Grandpa polishes off his bourbon)", // what will show up on the screen
+  use: 1 // how many time in a round this move can be used
+};
+
 module.exports = DrunkGrandpa;
