fa009c6e9e62718acc75f9e1b0c43986a3354f7e
Dave Harms <daveharmswebdev@gmail.com>
2016-07-06 18:31:24 -0500

    added images aboutus.jade and build.jade

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ const $ = require('jQuery');
 const header = require('../views/header.jade');
 const buildScreen = require('../views/buildScreen.jade');
 const rules = require('../views/rules.jade');
+const aboutUs = require('../views/aboutUs.jade');
 
+let family = [
+	'Grandma',
+	'Uncle Bobby',
+	'Jimmy',
+	'Sue',
+	'Beth',
+	'Diane'
+];
 
 $(function() {
 
@@ -13,12 +22,21 @@ $(function() {
 
 	$('#rules').on('click', function() {
 		toggleActive(this);
+		$('.feast').css("background-image", "url('../../src/images/thanksgiving-rockwell.jpg')")
+		$('div.append-point').empty();
+		$('div.append-point').append(rules({}));
 	});
 	$('#start').on('click', function() {
 		toggleActive(this);
+		$('.feast').css("background-image", "url('../../src/images/uncle_sam.jpg')")
+		$('div.append-point').empty();
+		$('div.append-point').append(buildScreen({family: family}));
 	});
 	$('#about').on('click', function() {
 		toggleActive(this);
+		$('.feast').css("background-image", "url('../../src/images/phones.jpg')");
+		$('div.append-point').empty();
+		$('div.append-point').append(aboutUs({}));
 	});
 
 	function toggleActive(link) {
